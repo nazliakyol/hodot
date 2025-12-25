dfa35217cecc42e417153c5a1f116758e892fcf2
Nazlı Akyol <nazlinurakyol@yahoo.com>
2025-12-25 21:54:47 +0100

    last_changes_25_Dec

---
+++ enemies/brian_the_bat.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+const SPEED = 30
+
+@export var range: = 128
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+@onready var ray_cast_2d : RayCast2D = $RayCast2D
+
+func _physics_process(delta: float) -> void :
+	var state = playback.get_current_node()
+	match state:
+		"Idle": pass
+		"Chase":
+			var player = get_player()
+			if player is Player:
+				velocity = global_position.direction_to(player.global_position) * SPEED
+				sprite_2d.scale.x = sign(velocity.x)
+			else:
+				velocity = Vector2.ZERO
+			move_and_slide()			
+
+func get_player() -> Player :
+	return get_tree().get_first_node_in_group('player')
+
+func is_player_in_range() -> bool :
+	var result = false
+	var player: = get_player()
+	if player is Player : 
+		var distance_to_player = global_position.distance_to(player.global_position)
+		if distance_to_player < range : 
+			result = true
+			
+	return result
+
+func can_see_player() -> bool :
+	if not is_player_in_range() : return false
+	var player = get_player()
+	ray_cast_2d.target_position = player.global_position - global_position
+	var has_los_to_player = not ray_cast_2d.is_colliding()
+	return has_los_to_player
