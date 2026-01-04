62d0ee9070ae0bc66c2ef8bc95009268d1e9ab5e
Nazlı Akyol <nazlinurakyol@yahoo.com>
2026-01-04 19:32:47 +0100

    4_jan_I

--- damage_boxes/hitbox.gd
+++ damage_boxes/hitbox.gd
@@ -3,3 +3,9 @@ class_name HitBox extends Area2D
 @export var damage: = 1
 @export var knockback_amount = 200
 @export var knockback_direction: Vector2
+@export var stores_hit_targets: bool = false
+
+var hit_targets : Array
+
+func clear_hit_targets() -> void :
+	hit_targets.clear()
--- damage_boxes/hurtbox.gd
+++ damage_boxes/hurtbox.gd
@@ -7,4 +7,7 @@ func _ready() -> void:
 
 func on_area_entered(area_2d : Area2D) -> void:
 	if area_2d is not HitBox: return
-	hurt.emit(area_2d)
+	var hitbox = area_2d as HitBox
+	if self in hitbox.hit_targets: return
+	if hitbox.stores_hit_targets : hitbox.hit_targets.append(self) 
+	hurt.emit(hitbox)
